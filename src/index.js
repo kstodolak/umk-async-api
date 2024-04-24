80fa8f57c6723153b643c09e5438d8c56063d73e
Kacper Stodolak <kacper.stodolak@allegro.com>
2024-04-24 14:09:56 +0200

    /newsletter POST endpoint

--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+
+app.post('/newsletter', (req, res) => {
+  if (Math.random() < 0.5) {
+    res.status(200).send('OK');
+  } else {
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 app.get('/questions', (req, res) => {
   if (req.query.id) {
     return res.status(404).send('No question found');
